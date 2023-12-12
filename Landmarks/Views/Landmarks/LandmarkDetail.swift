ef6b2f317368e62fbc538655845e8d6b5cddab99
Ivan Almada <ivan.almada@me.com>
2023-12-12 12:10:09 +0100

    Added Favorite button to Landmark detail

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 struct LandmarkDetail: View {
 
+    @Environment(ModelData.self) var modelData
+
     var landmark: Landmark
 
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+
     var body: some View {
+        @Bindable var modelData = modelData
+
         ScrollView {
             VStack {
                 MapView(coordinate: landmark.locationCoordinate)
@@ -22,8 +30,11 @@ struct LandmarkDetail: View {
                     .padding(.bottom, -130)
 
                 VStack(alignment: .leading) {
-                    Text(landmark.name)
-                        .font(.title)
+                    HStack {
+                        Text(landmark.name)
+                            .font(.title)
+                        FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                    }
                     HStack {
                         Text(landmark.park)
                             .font(.subheadline)
@@ -46,9 +57,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    let landmarks = ModelData().landmarks
-    return Group {
-        LandmarkDetail(landmark: landmarks[0])
-        LandmarkDetail(landmark: landmarks[1])
-    }
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
 }
