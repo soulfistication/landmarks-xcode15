9bdafc27ecfd678d3cb44b84c21baf7782f91e5c
Ivan Almada <ivan.almada@me.com>
2023-12-12 12:00:48 +0100

    Add Favorite button

---
+++ Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Iván Almada on 12/12/23.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
